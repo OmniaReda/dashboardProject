21d4e6c0fcf61390351c5476a574469b0a4d0075
OmniaReda <omniareda49@gmail.com>
2025-03-29 23:11:49 +0200

    change base apis for leadership component

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -8,28 +8,28 @@ export class DashboardService {
 
   async getHeaderStatistics() {
     const data = await fetch(
-      'https://78e85d90-4d6b-4c15-86c8-7303cc6a33a3.mock.pstmn.io/api-gateway-odoo/api/Statistics/HeaderStatistics'
+      'https://quilled-autumn-move.glitch.me/api-gateway-odoo/api/Statistics/CasesStatistics'
     );
     return (await data.json()) ?? [];
   }
 
   async getIncoming() {
     const data = await fetch(
-      'https://78e85d90-4d6b-4c15-86c8-7303cc6a33a3.mock.pstmn.io/GraphAPI/api/Graph/GetIncoming'
+      'https://quilled-autumn-move.glitch.me/GraphAPI/api/Graph/GetIncoming'
     );
     return (await data.json()) ?? [];
   }
 
   async getOutgoing() {
     const data = await fetch(
-      'https://78e85d90-4d6b-4c15-86c8-7303cc6a33a3.mock.pstmn.io/GraphAPI/api/Graph/GetOutgoing'
+      'https://quilled-autumn-move.glitch.me/GraphAPI/api/Graph/GetOutgoing'
     );
     return (await data.json()) ?? [];
   }
 
   async getInternal() {
     const data = await fetch(
-      'https://78e85d90-4d6b-4c15-86c8-7303cc6a33a3.mock.pstmn.io/GraphAPI/api/Graph/GetInternal'
+      'https://quilled-autumn-move.glitch.me/GraphAPI/api/Graph/GetInternal'
     );
     return (await data.json()) ?? [];
   }
